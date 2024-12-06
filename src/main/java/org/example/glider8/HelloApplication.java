d15328b1bb0908180373f1b22bab3053d47c2b3e
Ruth_Nure <rnure1@student.gsu.edu>
2024-12-06 13:51:01 -0500

    Splash/main FXML + Helloapplication

--- src/main/java/org/example/glider8/HelloApplication.java
+++ src/main/java/org/example/glider8/HelloApplication.java
@@ -1,23 +1,47 @@
 package org.example.glider8;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.Parent;
+import javafx.animation.PauseTransition;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!!");
-        stage.setScene(scene);
+        FXMLLoader splashLoader = new FXMLLoader(getClass().getResource("Splash Screen.fxml"));
+
+        Parent splashRoot = splashLoader.load();
+        Scene splashScene = new Scene(splashRoot, 600, 400);
+
+        stage.setScene(splashScene);
+
+        stage.setTitle("Welcome to Glider!");
         stage.show();
+
+        PauseTransition delay = new PauseTransition(Duration.seconds(5));
+        delay.setOnFinished(event -> {
+            try {
+                // Load the Main Menu FXML after the splash
+                FXMLLoader mainMenuLoader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+                Parent mainMenuRoot = mainMenuLoader.load();
+                Scene mainMenuScene = new Scene(mainMenuRoot);
+
+                stage.setScene(mainMenuScene);
+                stage.setTitle("Main Menu");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        delay.play();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
